24b961e46cd183cf77ae0bc3d3ad1929a5f6c5ea
UmaKim <85341050+umaKim@users.noreply.github.com>
2023-08-01 17:57:10 +0900

    Initial Commit

---
+++ AQX/AQXApp.swift
@@ -0,0 +1,17 @@
+//
+//  AQXApp.swift
+//  AQX
+//
+//  Created by 김윤석 on 2023/08/01.
+//
+
+import SwiftUI
+
+@main
+struct AQXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AQX/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AQX
+//
+//  Created by 김윤석 on 2023/08/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
