8dc9f024668de9f09d2e77884e3b3391ee7887f1
UmaKim <85341050+umaKim@users.noreply.github.com>
2023-08-17 09:17:19 +0900

    set up rootView

--- AQX/AQXApp.swift
+++ AQX/AQXApp.swift
@@ -4,14 +4,27 @@
 //
 //  Created by 김윤석 on 2023/08/01.
 //
-
+import ComposableArchitecture
 import SwiftUI
+import UIKit
 
 @main
 struct AQXApp: App {
+    init() {
+        
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            RootView(store:
+                    .init(
+                        initialState: RootReducer.State(),
+                        reducer: {
+                            RootReducer()
+                                ._printChanges()
+                        }
+                    )
+            )
         }
     }
 }
