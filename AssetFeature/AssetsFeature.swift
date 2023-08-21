2efddff49c595c3699d8f2e290533f1ec1a2cdf2
UmaKim <85341050+umaKim@users.noreply.github.com>
2023-08-22 00:17:00 +0900

    set up AssetFeature module

---
+++ AssetFeature/AssetsFeature.swift
@@ -0,0 +1,18 @@
+//
+//  AssetsFeature.swift
+//  AQX
+//
+//  Created by 김윤석 on 2023/08/02.
+//
+
+import SwiftUI
+
+public struct AssetsFeatureView: View {
+    public init() { }
+    
+    public var body: some View {
+        VStack {
+            Text("Assets")
+        }
+    }
+}
