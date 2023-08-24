d431eb6bbc2991c8fb08a337e3366729ec5f3de1
UmaKim <85341050+umaKim@users.noreply.github.com>
2023-08-24 15:03:19 +0900

    modularize Models

---
+++ Models/Models.h
@@ -0,0 +1,18 @@
+//
+//  Models.h
+//  Models
+//
+//  Created by 김윤석 on 2023/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Models.
+FOUNDATION_EXPORT double ModelsVersionNumber;
+
+//! Project version string for Models.
+FOUNDATION_EXPORT const unsigned char ModelsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Models/PublicHeader.h>
+
+
