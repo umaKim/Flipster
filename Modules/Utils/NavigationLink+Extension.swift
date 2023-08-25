ab3bda381e7dc44a0e173bc37d01bde29b84be9b
UmaKim <85341050+umaKim@users.noreply.github.com>
2023-08-26 03:04:10 +0900

    set up NavigationLink Extension

---
+++ Modules/Utils/NavigationLink+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  NavigationLink+Extension.swift
+//  AQX
+//
+//  Created by 김윤석 on 2023/08/19.
+//
+import SwiftUI
+
+extension NavigationLink where Label == EmptyView {
+    public init<T: Hashable>(destination: Destination, when selection: Binding<T?>, equals tag: T) {
+        self.init(
+            tag: tag,
+            selection: selection,
+            destination:{ destination },
+            label: { EmptyView() }
+        )
+
+    }
+}
