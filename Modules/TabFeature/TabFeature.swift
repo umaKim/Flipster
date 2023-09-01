b13c77cabfdc6b3e9071669f0e9f34ec513a529b
UmaKim <85341050+umaKim@users.noreply.github.com>
2023-09-01 09:44:25 +0900

    [FEAT] Set up TabFeatureView

---
+++ Modules/TabFeature/TabFeature.swift
@@ -0,0 +1,52 @@
+//
+//  TabFeature.swift
+//  AQX
+//
+//  Created by 김윤석 on 2023/08/02.
+//
+import Service
+import TradeFeature
+import AssetFeature
+import SwiftUI
+
+enum Tab: String, Equatable {
+    case trade
+    case asset
+    
+    @ViewBuilder
+    var label: some View {
+        switch self {
+        case .trade:
+            Label("Trade", systemImage: "chart.bar.fill")
+        case .asset:
+            Label("Asset", systemImage: "creditcard")
+        }
+    }
+}
+
+public struct TabFeatureView: View {
+    public init() {}
+    
+    private let tradeFeatureViewModel = TradeFeatureViewModel(
+        repository:
+            TradeRepositoryImp(
+                networkManager: RESTApiManager(),
+                websocket: WebSocketApiManager()
+            )
+    )
+    
+    public var body: some View {
+        TabView {
+            NavigationStack {
+                TradeFeatureView(tradeFeatureViewModel)
+            }
+            .tabItem { Tab.trade.label }
+            
+            NavigationStack {
+                AssetsFeatureView()
+            }
+            .tabItem { Tab.asset.label }
+        }
+    }
+}
+
