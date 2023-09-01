d2c18a2442eee64388fb24a7d3d7ab4e66d2298f
UmaKim <85341050+umaKim@users.noreply.github.com>
2023-09-01 10:54:59 +0900

    [TEST] Set up TradeFeatureTests
    - add test_MostTraded
    - add test_TopMovers
    - add test_FilteredCryptos
    - add test_FilteredCryptosWhenSearching
    - add test_WsValue
    - add test_selectedCrypto_whenOnAppearIsCalled
    - add test_nextState_whenOnAppearIsCalled
    - add test_OnDisappear

--- Modules/TradeFeatureTests/TradeFeatureTests.swift
+++ Modules/TradeFeatureTests/TradeFeatureTests.swift
@@ -4,45 +4,96 @@
 //
 //  Created by 김윤석 on 2023/08/20.
 //
-
+import Mock
+import Models
+import Combine
 import Service
 import XCTest
 @testable import TradeFeature
 
 final class TradeFeatureTests: XCTestCase {
-
-    var vm: TradeFeatureViewModel?
+    
+    var vm: TradeFeatureViewModel!
     
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
         vm = TradeFeatureViewModel(
-            socketService: RealTimeCoinPriceRepositoryImp(StarScreamWebSocket()),
-            restApiService: <#T##NetworkManager#>
+            repository: MockTradeViewRepositoryImp(
+                networkManager: MockRESTApiManager(),
+                websocket: MockWebSocketApiManger()
+            )
         )
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         vm = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_MostTraded() {
+        let expectation = expectation(description: "Fetch mostTraded data")
         
-        vm?.onAppear()
-        
-        vm?.onDisappear()
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            XCTAssertEqual(self.vm.mostTraded, [MockData.crypto])
+            expectation.fulfill()
         }
+        waitForExpectations(timeout: 5, handler: nil)
     }
-
+    
+    func test_TopMovers() {
+        let expectation = expectation(description: "Fetch topMovers data")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            XCTAssertEqual(self.vm.topMovers, [MockData.crypto])
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func test_FilteredCryptos() {
+        let expectation = expectation(description: "Fetch filteredCryptos data")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            XCTAssertEqual(self.vm.filteredCryptos, [MockData.crypto])
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func test_FilteredCryptosWhenSearching() {
+        let expectation = expectation(description: "Fetch mostTraded data")
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.vm.viewStatus = .searching
+            self.vm.searchText = "b"
+            XCTAssertEqual(self.vm.mostTraded, [MockData.crypto])
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func test_WsValue() {
+        let expectation = expectation(description: "Fetch changed ws coin price")
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            XCTAssertEqual(
+                self.vm.filteredCryptos[0].currentPrice,
+                MockData.coinPriceData.price
+            )
+            print(self.vm.filteredCryptos[0].currentPrice)
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 4, handler: nil)
+    }
+    
+    func test_selectedCrypto_whenOnAppearIsCalled() {
+        vm.onAppear()
+        XCTAssertNil(vm.selectedCrypto)
+    }
+    
+    func test_nextState_whenOnAppearIsCalled() {
+        vm.onAppear()
+        XCTAssertNil(vm.nextState)
+    }
+    
+    func test_OnDisappear() {
+        vm.onDisappear()
+    }
 }
