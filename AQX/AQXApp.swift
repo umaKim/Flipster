32f1989c3c3f7466e2d3a05bd198a806c52c0e2b
UmaKim <85341050+umaKim@users.noreply.github.com>
2023-09-02 01:59:19 +0900

    [FEAT] Set up ContentView

--- AQX/AQXApp.swift
+++ AQX/AQXApp.swift
@@ -4,15 +4,14 @@
 //
 //  Created by 김윤석 on 2023/08/01.
 //
-import Kingfisher
+
 import SwiftUI
-import TabFeature
 
 @main
 struct AQXApp: App {
     var body: some Scene {
         WindowGroup {
-            TabFeatureView()
+           ContentView()
         }
     }
 }
---
+++ AQX/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  AQX
+//
+//  Created by 김윤석 on 2023/09/01.
+//
+
+import TabFeature
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabFeatureView()
+    }
+}
