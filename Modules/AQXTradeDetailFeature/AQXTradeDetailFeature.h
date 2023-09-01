31348aee266d22039e3b65bc271dbc5cad775b6c
UmaKim <85341050+umaKim@users.noreply.github.com>
2023-09-01 09:29:35 +0900

    [FEAT] Set up AQXTradingDetailFeature module

---
+++ Modules/AQXTradeDetailFeature/AQXTradeDetailFeature.h
@@ -0,0 +1,18 @@
+//
+//  AQXTradeDetailFeature.h
+//  AQXTradeDetailFeature
+//
+//  Created by 김윤석 on 2023/08/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AQXTradeDetailFeature.
+FOUNDATION_EXPORT double AQXTradeDetailFeatureVersionNumber;
+
+//! Project version string for AQXTradeDetailFeature.
+FOUNDATION_EXPORT const unsigned char AQXTradeDetailFeatureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AQXTradeDetailFeature/PublicHeader.h>
+
+
