659708d8e7034933bc5b3bb248042fd2683603fe
UmaKim <85341050+umaKim@users.noreply.github.com>
2023-09-01 09:40:16 +0900

    [FEAT] Set up Service module

---
+++ Modules/Service/Service.h
@@ -0,0 +1,18 @@
+//
+//  Service.h
+//  Service
+//
+//  Created by 김윤석 on 2023/08/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Service.
+FOUNDATION_EXPORT double ServiceVersionNumber;
+
+//! Project version string for Service.
+FOUNDATION_EXPORT const unsigned char ServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Service/PublicHeader.h>
+
+
