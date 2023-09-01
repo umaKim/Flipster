5e86b5db75cbb25d7b113dc09545172707d210b5
UmaKim <85341050+umaKim@users.noreply.github.com>
2023-09-01 09:32:28 +0900

    [FEAT] Set up AQXTradingDetailRepositoryImp

---
+++ Modules/AQXTradeDetailFeature/ViewModel/AQXTradingDetailRepository.swift
@@ -0,0 +1,26 @@
+//
+//  AQXTradingDetailRepository.swift
+//  AQXTradeDetailFeature
+//
+//  Created by 김윤석 on 2023/08/31.
+//
+import Models
+import Service
+import Foundation
+
+public protocol AQXTradingDetailRepository {
+    func fetchChartData(url: URL) async -> Result<Models.ChartData, APIError>
+}
+
+public final class AQXTradingDetailRepositoryImp: AQXTradingDetailRepository {
+   
+    private let networkManager: RESTApiProtocol
+    
+    public init(networkManager: RESTApiProtocol) {
+        self.networkManager = networkManager
+    }
+    
+    public func fetchChartData(url: URL) async -> Result<Models.ChartData, Service.APIError> {
+        return await networkManager.request(url: url, expecting: Models.ChartData.self)
+    }
+}
