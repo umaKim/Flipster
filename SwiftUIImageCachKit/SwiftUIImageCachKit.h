59c04467a71fbd49a133a2f79e9bfe4d9587f212
UmaKim <85341050+umaKim@users.noreply.github.com>
2023-09-04 21:54:27 +0900

    [CHORE] Set up SwiftUIImageCachKit module

---
+++ SwiftUIImageCachKit/SwiftUIImageCachKit.h
@@ -0,0 +1,18 @@
+//
+//  SwiftUIImageCachKit.h
+//  SwiftUIImageCachKit
+//
+//  Created by 김윤석 on 2023/09/04.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftUIImageCachKit.
+FOUNDATION_EXPORT double SwiftUIImageCachKitVersionNumber;
+
+//! Project version string for SwiftUIImageCachKit.
+FOUNDATION_EXPORT const unsigned char SwiftUIImageCachKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUIImageCachKit/PublicHeader.h>
+
+
